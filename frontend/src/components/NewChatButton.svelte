a389fb675f8085281c55354c6f047cc1613ddee2
Kyle Dampier <kyle@dampier.io>
2023-03-04 20:57:58 -0500

    added get uid from localstorage

---
+++ frontend/src/components/NewChatButton.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<button> <span class="icon">+</span>New Chat</button>
+
+<style>
+  button {
+    background-color: #373737;
+    color: white;
+    border: 1px solid #404040;
+    border-radius: 0.5rem;
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 0.5em;
+    margin: 1em 2em;
+    font-size: 1rem;
+    transition: all 0.5s ease-in-out;
+  }
+
+  button:hover {
+    background-color: #404040;
+    filter: drop-shadow(0px 8px 5px #222);
+  }
+
+  .icon {
+    font-size: 2rem;
+    margin-right: 0.5rem;
+  }
+</style>
