1a1f78812c286e206be15f2c92ade77490e9167a
DaniParr <daniel27parra@gmail.com>
2023-03-04 22:33:29 -0500

    "added landing page"

---
+++ frontend/src/routes/landing/+page.svelte
@@ -0,0 +1,28 @@
+<div>
+    <h1>Welcome to the Neural Network</h1>
+    <div>
+        <p>The Neural Networks is a safe space that reminds you that you are not alone, where you can feel comfortable and supported, even during difficult times. It's a virtual space where you can express yourself freely without fear of judgment, criticism, or discrimination.</p>
+        <img src="./static/landing1.png" alt="image">      
+    </div>
+    
+    <p>This safe space is designed to provide a sense of community and belonging, where individuals can come together to share their experiences, thoughts, and emotions anonymously without feeling isolated or disconnected. It's a place where you can find others who have been through similar struggles and can offer support and understanding.</p>
+
+    <p>It's a reminder that you are not alone in your struggles and that there are others who care about you and are ready to support you on your journey.</p>
+
+    <button>Start Networking</button>
+</div>
+
+<style>
+    h1
+    {
+        font-size: 64px;
+    }
+    div
+    {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        justify-content: center;
+        padding: 30px
+    }
+</style>
